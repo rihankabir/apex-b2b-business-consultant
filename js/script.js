92ff916aa941c53875fe126d3cea17d460287723
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-08 01:59:15 +0600

    removed unnessary comment's from each file

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@ document.querySelectorAll(".brand").forEach((logo) => {
 });
 const goTop = document.getElementById("goTop");
 
-// Show button after scrolling 300px
 window.addEventListener("scroll", () => {
     if (window.scrollY > 300) {
         goTop.style.display = "flex";
@@ -28,7 +27,6 @@ window.addEventListener("scroll", () => {
     }
 });
 
-// Scroll smoothly to top
 goTop.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
